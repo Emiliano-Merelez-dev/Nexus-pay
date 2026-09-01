512d866f917f6927565853ef80747e0ef2b8154e
Emiliano Merelez <emimerelez@gmail.com>
2026-09-01 20:48:35 -0300

    feat(merchants): implement MerchantsController endpoints for creation and retrieval

---
+++ src/merchants/merchants.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { MerchantsService } from './merchants.service';
+import { CreateMerchantDto } from './dto/create-merchant.dto';
+
+@Controller('merchants')
+export class MerchantsController {
+  constructor(private readonly merchantsService: MerchantsService) {}
+
+  @Post()
+  create(@Body() createMerchantDto: CreateMerchantDto) {
+    return this.merchantsService.create(createMerchantDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.merchantsService.findAll();
+  }
+}
